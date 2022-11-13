35c9bcf9e11f4642c968e0d6bb0b15db57e6cfe2
ILYAPOPOFF <IlyaP1998@gmail.com>
2022-11-14 00:43:23 +0400

    Add butoon "Exit" in Profile

--- mapKebab/View/ProfileViewController.swift
+++ mapKebab/View/ProfileViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    //Button - Exit Profile
+    let buttonExit : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "door.left.hand.open"), for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        setupView()
+        
     }
-
-
+    
+    func setupView() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: buttonExit)
+        
+    }
 }
