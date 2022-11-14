82a8af709bd5475a24653bd0e01257d719d2d603
666kurt <incentivedie@gmail.com>
2022-11-15 00:14:05 +0400

    added subtitle and button for WelcomeViewController

--- mapKebab/View/WelcomeViewController.swift
+++ mapKebab/View/WelcomeViewController.swift
@@ -37,13 +37,29 @@ class WelcomeViewController: UIViewController {
         return label
     }()
     
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.spacing = 2
-        stackView.axis = .vertical
-        stackView.distribution = .equalCentering
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
+    private let stackView = UIStackView.setStackView(axis: .vertical, spacing: 2, distribution: .equalCentering)
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Войдите, чтобы управлять своими избранными, делиться отзывами и другими полезными вещами"
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 11)
+        label.textColor = #colorLiteral(red: 0.6274509804, green: 0.6274509804, blue: 0.6274509804, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let joinButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 10
+        button.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.4980392157, blue: 0.2235294118, alpha: 1)
+        button.setTitle("Войти или зарегистрироваться", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(joinButtonTapped), for: .touchUpInside)
+        return button
     }()
 
     override func viewDidLoad() {
@@ -54,12 +70,19 @@ class WelcomeViewController: UIViewController {
  
     }
     
+    @objc private func joinButtonTapped() {
+        print("tap")
+    }
+    
     private func setupView() {
         
         title = "Profile"
         
         view.addSubview(logoImage)
         view.addSubview(stackView)
+        view.addSubview(subTitleLabel)
+        view.addSubview(joinButton)
+        
         [nameApp, labelWelcome].forEach { view in
             stackView.addArrangedSubview(view)
         }
@@ -75,7 +98,16 @@ class WelcomeViewController: UIViewController {
             logoImage.heightAnchor.constraint(equalToConstant: 230),
             
             stackView.topAnchor.constraint(equalTo: logoImage.topAnchor, constant: 144),
-            stackView.centerXAnchor.constraint(equalTo: logoImage.centerXAnchor)
+            stackView.centerXAnchor.constraint(equalTo: logoImage.centerXAnchor),
+            
+            subTitleLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 76),
+            subTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 33),
+            subTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -33),
+            
+            joinButton.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 22),
+            joinButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            joinButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            joinButton.heightAnchor.constraint(equalToConstant: 48)
         
         ])
     }
